9fb24196ed15dd2df99e001a3172c995078c543d
Shirley <smbiayuk@gmail.com>
2024-08-29 11:38:36 +0200

    files added

---
+++ src/main/java/WTproject/boekenWT/BoekenWtApplication.java
@@ -0,0 +1,13 @@
+package WTproject.boekenWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekenWtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoekenWtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WTproject/boekenWT/BoekenWtApplicationTests.java
@@ -0,0 +1,13 @@
+package WTproject.boekenWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoekenWtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
