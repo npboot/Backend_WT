08bcf52130e1242cc8ac2b721729d8d9c2d92b88
AmamiSQ <r_zimmerman@outlook.com>
2024-09-03 12:01:53 +0200

    added create book function

--- src/main/java/WTproject/boekenWT/logic/CreateBook.java
+++ src/main/java/WTproject/boekenWT/logic/CreateBook.java
@@ -1,10 +1,14 @@
 package WTproject.boekenWT.logic;
 
 import WTproject.boekenWT.models.Author;
+import WTproject.boekenWT.models.AuthorRepository;
 import WTproject.boekenWT.models.Book;
 import WTproject.boekenWT.models.BookRepository;
+import WTproject.boekenWT.models.JsonClass;
 import WTproject.boekenWT.models.KnowledgeSource;
 import WTproject.boekenWT.models.PhysicalBook;
+
+import java.time.Year;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @SpringBootApplication
@@ -21,31 +28,54 @@ public class CreateBook {
 
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
-    private Author author;
-    private Book book;
-    private KnowledgeSource source;
-    private PhysicalBook physicalBook;
-
-    public void createBook() {
-        
+    public void createBook(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     @PostMapping("/addbook")
-    public String addBook(@RequestBody Book book) {
-        String title = "Java for dummies";
-        String isbn = "978-90-430-4185-1";
-        String authorName = "Barry A. Burd";
-        String authorSurname = "Burd";
-        String authorId = "1";
-        String year = "2019";
-        String sourceId = "1";
-        String sourceType = "Book";
-        String physicalBookId = "1";
+    public String addBook(@RequestBody JsonClass json) {
+        Book book = new Book();
+        Author author = new Author();
+        Year year = json.getYear();
 
-        
+        Author newAuthor = json.getAuthor();
+        Book newBook = json.getBook();
 
-        bookRepository.save(book);
+        author.setAuthorId(newAuthor.getAuthorId());
+        author.setName(newAuthor.getName());
+
+        //check if author already exists
+        if (authorRepository.existsById(author.getAuthorId())) {
+            author = authorRepository.findById(author.getAuthorId()).get();
+        }
+        else {
+            try {
+                authorRepository.save(author);
+            } catch (Exception e) {
+                return "Error: " + e;
+            }
+        }
+
+        if(bookRepository.existsById(newBook.getIsbn())) {
+            return "Book already exists";
+        }
+        else {
+            try {
+                book.setIsbn(newBook.getIsbn());
+                book.setTitle(newBook.getTitle());
+                book.setAuthor(author);
+                book.setYear(year);
+
+                bookRepository.save(book);
+            } catch (Exception e) {
+                return "Error: " + e;
+            }
+        }
+
         return "Book added";
     }
     
