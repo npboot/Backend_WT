df7da136cb48a2f338df67f2341ccfc8e53f6d68
Shirley <smbiayuk@gmail.com>
2024-09-17 11:00:37 +0200

    Tabellen aangemaakt: Request, RequestStatus, Borrowing

--- src/main/java/WTproject/boekenWT/models/Borrowing.java
+++ src/main/java/WTproject/boekenWT/models/Borrowing.java
@@ -5,18 +5,21 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table
+@Table(name="BORROWING")
 public class Borrowing {
 
+    //attributes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int borrowingId;
 
-//    @OneToOne
-//    @JoinColumn()
+    @OneToOne
+    @JoinColumn(name = "requestId")
+    private Request request;
 
-    @Column
-    private int copyId;
+    @ManyToOne
+    @JoinColumn(name = "copyId")
+    private PhysicalBookCopy physicalBookCopy;
 
     @Column
     private Date startDate;
@@ -24,7 +27,52 @@ public class Borrowing {
     @Column
     private Date returnDate;
 
-    @Column
-    private int borrowingStatusId;
+    @ManyToOne
+    @JoinColumn(name = "borrowingStatusId")
+    private BorrowingStatus borrowingStatus;
 
+    //getters and setters
+    public int getBorrowingId() {
+        return borrowingId;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public PhysicalBookCopy getPhysicalBookCopy() {
+        return physicalBookCopy;
+    }
+
+    public void setPhysicalBookCopy(PhysicalBookCopy physicalBookCopy) {
+        this.physicalBookCopy = physicalBookCopy;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public BorrowingStatus getBorrowingStatus() {
+        return borrowingStatus;
+    }
+
+    public void setBorrowingStatus(BorrowingStatus borrowingStatus) {
+        this.borrowingStatus = borrowingStatus;
+    }
 }
--- src/main/java/WTproject/boekenWT/models/Request.java
+++ src/main/java/WTproject/boekenWT/models/Request.java
@@ -5,9 +5,10 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table
+@Table(name="REQUEST")
 public class Request {
 
+    //attributes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int requestId;
@@ -17,13 +18,50 @@ public class Request {
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "bookId")
+    @JoinColumn(name = "pBookId")
     private PhysicalBook physicalBook;
 
     @Column
     private Date requestDate;
 
-    @Column
-    private int requestStatusId;
+    @ManyToOne
+    @JoinColumn(name = "requestStatusId")
+    private RequestStatus requestStatus;
 
+    //getters and setters
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public PhysicalBook getPhysicalBook() {
+        return physicalBook;
+    }
+
+    public void setPhysicalBook(PhysicalBook physicalBook) {
+        this.physicalBook = physicalBook;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public RequestStatus getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(RequestStatus requestStatus) {
+        this.requestStatus = requestStatus;
+    }
 }
