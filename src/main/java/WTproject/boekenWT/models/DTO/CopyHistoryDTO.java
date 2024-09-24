e7933e2373ac970bbd056892c210776ee32789b7
Shirley <smbiayuk@gmail.com>
2024-09-24 18:35:19 +0200

    CopyHistoryDTO aangemaakt

---
+++ src/main/java/WTproject/boekenWT/models/DTO/CopyHistoryDTO.java
@@ -0,0 +1,22 @@
+package WTproject.boekenWT.models.DTO;
+
+public class CopyHistoryDTO {
+    private String date;
+    private String action;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
